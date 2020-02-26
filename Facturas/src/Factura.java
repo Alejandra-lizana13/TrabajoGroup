106bbd543d57a4286bbd91c5f2959b7f0d207917
Andrei Alexandru Mois <andreitarner103@gmail.com>
2020-02-26 19:53:54 +0100

    clase factura

---
+++ Facturas/src/Factura.java
@@ -0,0 +1,26 @@
+
+public class Factura implements IFactura {
+	public int num;
+	public float base;
+	public float tipoIva;
+	public Estados estado;
+	
+	public static void main(String[] args) {
+
+	}
+
+
+	public void cambiarEstado(Estados estado) {		
+	}
+
+	
+	public float getTotal() {
+		return 0;
+	}
+
+	
+	public float getCantidadIva() {
+		return 0;
+	}
+
+}
